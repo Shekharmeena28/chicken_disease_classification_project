2c511c6a96d0c6a041e4e5020d51d891a4652eec
shekhar <shekharmeena48@gmail.com>
2023-10-16 22:25:51 +0530

    dvc added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -17,7 +17,7 @@ except Exception as e:
 
 
 
-STAGE_NAME = "Prepare base model training pipeline"
+STAGE_NAME = "Prepare base model training pipeline Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = PrepareBaseModelTrainingPipeline()
@@ -29,10 +29,21 @@ except Exception as e:
 
 
 
-STAGE_NAME = "Training"
+STAGE_NAME = "Training Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = ModelTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = EvaluationPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
